91c55e5865fe0b86cdec15ea72b48540f0a932cd
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-05-24 10:20:50 +0300

    Improve run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-time python3 online_preprocessing/run_all_subjects.py
-time python3 train_transfer.py -c configs/replicate_prime.yaml | tee replicate_prime.log
+python3 -m venv .venv && source .venv/bin/activate
+time python3 online_preprocessing/run_all_subjects.py | tee preprocessing.log
+time python3 train_transfer.py -c configs/replicate_prime.yaml | tee training.log
