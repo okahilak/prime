2c0b594a7b6fb9f8d0befcd00ee3461c870486c1
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-05-28 11:52:55 +0300

    Convenience changes to run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 python3 -m venv .venv && source .venv/bin/activate
-time python3 online_preprocessing/run_all_subjects.py | tee preprocessing.log
+
+read -rp "Run preprocessing? [y/N] " run_preprocessing
+if [[ "$run_preprocessing" =~ ^[Yy]$ ]]; then
+    time python3 online_preprocessing/run_all_subjects.py | tee preprocessing.log
+fi
 time python3 train_transfer.py -c configs/replicate_prime.yaml | tee training.log
+time python3 train_transfer.py -c configs/replicate_prime_short.yaml | tee training.log
