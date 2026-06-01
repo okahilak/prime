7e1a8b48645b0a112bccb7051e1237bad65cc0c7
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-06-01 10:28:27 +0300

    Remove venv init from run.sh

--- prime/run.sh
+++ prime/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-python3 -m venv .venv && source .venv/bin/activate
-
 read -rp "Run preprocessing? [y/N] " run_preprocessing
 if [[ "$run_preprocessing" =~ ^[Yy]$ ]]; then
     python3 online_preprocessing/run_all_subjects.py
