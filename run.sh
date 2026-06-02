c388b467ae4da93243ae04004b5f6d4fbdb18222
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-06-02 14:07:40 +0300

    Use only subject 21 as a test

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 read -rp "Run preprocessing? [y/N] " run_preprocessing
 if [[ "$run_preprocessing" =~ ^[Yy]$ ]]; then
-    python3 online_preprocessing/run_all_subjects.py
+    python3 -m prime.online_preprocessing.run_all_subjects
 fi
 python3 -m prime.train --cv
 
@@ -10,10 +10,14 @@ python3 -m prime.train --cv
 python3 -m prime.train --train 18 19 20 23 25 28 29 35 40 42 43 44 45 50 101 102 107 108 110 111 113 115 116 120 125
 
 # Test datasets from the first fold of the cross-validation split
-python3 -m prime.train --test 21 22 24 26 27 31 34 36 38
+
+#test_subjects="21 22 24 26 27 31 34 36 38"
+test_subjects="21"
+
+python3 -m prime.train --test $test_subjects
 
 # Loop over test datasets
-for subject_id in 21 22 24 26 27 31 34 36 38; do
+for subject_id in $test_subjects; do
     python3 -m prime.simulate_online "$subject_id"
     exit_code=$?
     if [[ $exit_code -ne 0 ]]; then
