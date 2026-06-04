59d5c74675e8b6ea37f73384ae64d4e81865785f
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-06-04 10:54:23 +0300

    Use all test subjects in run.sh

--- run.sh
+++ run.sh
@@ -11,8 +11,7 @@ python3 -m prime_core.train --train 18 19 20 23 25 28 29 35 40 42 43 44 45 50 10
 
 # Test datasets from the first fold of the cross-validation split
 
-#test_subjects="21 22 24 26 27 31 34 36 38"
-test_subjects="21"
+test_subjects="21 22 24 26 27 31 34 36 38"
 
 python3 -m prime_core.train --test $test_subjects
 
